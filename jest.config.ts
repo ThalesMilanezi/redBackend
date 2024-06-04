d9f90a033691e1eaddbffbcbe3e4bdd13c465c68
Thales Milanezi <milanezitf@gmail.com>
2024-06-04 10:16:25 -0300

    fix jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,13 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**',
-    '!<rootDir>/src/presentation/protocols/**',
-    '!<rootDir>/src/data/protocols/**',
-    '!<rootDir>/src/domain/models/**'
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
